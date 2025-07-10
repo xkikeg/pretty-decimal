3b7acd0c5c23967a4f6691415d121e14df8e9449
kikeg <kikeg@kikeg.com>
2025-07-10 17:08:12 +0200

    Renamed benchmark group.

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,19 +6,16 @@ use pretty_decimal::PrettyDecimal;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use rust_decimal_macros::dec;
 
-fn parse_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("parse");
+fn benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("pretty-decimal");
+
     group.bench_function(BenchmarkId::new("from_str", "plain"), |b| {
         b.iter(|| black_box(PrettyDecimal::from_str("12345.678").unwrap()))
     });
     group.bench_function(BenchmarkId::new("from_str", "comma"), |b| {
         b.iter(|| black_box(PrettyDecimal::from_str("12,345.678").unwrap()))
     });
-    group.finish();
-}
 
-fn to_string_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("to_stirng");
     group.bench_function(BenchmarkId::new("to_string", "plain"), |b| {
         b.iter(|| black_box(PrettyDecimal::plain(dec!(12_345_678.90)).to_string()))
     });
@@ -27,5 +24,5 @@ fn to_string_benchmark(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, parse_benchmark, to_string_benchmark);
+criterion_group!(benches, benchmark);
 criterion_main!(benches);
