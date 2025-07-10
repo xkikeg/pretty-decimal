19826aa7aae76ab078913cfb4941eb10c742f26d
kikeg <kikeg@kikeg.com>
2025-07-10 16:39:20 +0200

    Initial implementation.
    It was copied from
    https://github.com/xkikeg/okane/blob/658fce900deb6d1aa515edf7737551d651b3db7c/core/src/syntax/pretty_decimal.rs

---
+++ benches/bench.rs
@@ -0,0 +1,31 @@
+use std::hint::black_box;
+use std::str::FromStr;
+
+use pretty_decimal::PrettyDecimal;
+
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use rust_decimal_macros::dec;
+
+fn parse_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse");
+    group.bench_function(BenchmarkId::new("from_str", "plain"), |b| {
+        b.iter(|| black_box(PrettyDecimal::from_str("12345.678").unwrap()))
+    });
+    group.bench_function(BenchmarkId::new("from_str", "comma"), |b| {
+        b.iter(|| black_box(PrettyDecimal::from_str("12,345.678").unwrap()))
+    });
+    group.finish();
+}
+
+fn to_string_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("to_stirng");
+    group.bench_function(BenchmarkId::new("to_string", "plain"), |b| {
+        b.iter(|| black_box(PrettyDecimal::plain(dec!(12_345_678.90)).to_string()))
+    });
+    group.bench_function(BenchmarkId::new("to_string", "comma"), |b| {
+        b.iter(|| black_box(PrettyDecimal::comma3dot(dec!(12_345_678.90)).to_string()))
+    });
+}
+
+criterion_group!(benches, parse_benchmark, to_string_benchmark);
+criterion_main!(benches);
