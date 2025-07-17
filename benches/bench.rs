0380f5468a056a80ed8765e51d6877d755ec1165
kikeg <kikeg@kikeg.com>
2025-07-17 08:21:27 +0200

    Updated benchmark to use more diversed inputs.

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,25 +4,49 @@ use std::str::FromStr;
 use pretty_decimal::PrettyDecimal;
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
 
-fn benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("pretty-decimal");
+static TEST_CASES: &[(&'static str, Decimal)] = &[
+    ("regular", dec!(12_345_678.90)),
+    ("negative", dec!(-12_345_678.90)),
+    ("small", dec!(123)),
+    ("tiny", dec!(0.000000001)),
+];
 
-    group.bench_function(BenchmarkId::new("from_str", "plain"), |b| {
-        b.iter(|| black_box(PrettyDecimal::from_str("12345.678").unwrap()))
-    });
-    group.bench_function(BenchmarkId::new("from_str", "comma"), |b| {
-        b.iter(|| black_box(PrettyDecimal::from_str("12,345.678").unwrap()))
-    });
+fn from_str_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("from_str");
 
-    group.bench_function(BenchmarkId::new("to_string", "plain"), |b| {
-        b.iter(|| black_box(PrettyDecimal::plain(dec!(12_345_678.90)).to_string()))
-    });
-    group.bench_function(BenchmarkId::new("to_string", "comma"), |b| {
-        b.iter(|| black_box(PrettyDecimal::comma3dot(dec!(12_345_678.90)).to_string()))
-    });
+    for (name, value) in TEST_CASES {
+        let s = value.to_string();
+        group.bench_function(
+            BenchmarkId::new("PrettyDecimal", format!("plain-{}", name)),
+            |b| b.iter(|| black_box(PrettyDecimal::from_str(&s).unwrap())),
+        );
+        let s = PrettyDecimal::comma3dot(*value).to_string();
+        group.bench_function(
+            BenchmarkId::new("PrettyDecimal", format!("comma-{}", name)),
+            |b| b.iter(|| black_box(PrettyDecimal::from_str(&s).unwrap())),
+        );
+    }
+
+    group.finish();
 }
 
-criterion_group!(benches, benchmark);
+fn to_string_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("to_string");
+
+    for (name, value) in TEST_CASES {
+        group.bench_function(
+            BenchmarkId::new("PrettyDecimal", format!("plain-{}", name)),
+            |b| b.iter(|| black_box(PrettyDecimal::plain(*value).to_string())),
+        );
+        group.bench_function(
+            BenchmarkId::new("PrettyDecimal", format!("comma-{}", name)),
+            |b| b.iter(|| black_box(PrettyDecimal::comma3dot(*value).to_string())),
+        );
+    }
+}
+
+criterion_group!(benches, from_str_benchmark, to_string_benchmark);
 criterion_main!(benches);
